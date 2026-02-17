cfb6b76eb45e9300da28bdbeebf61492d1068109
unknown <f.aitbaram0097@uca.ac.ma>
2026-02-18 00:28:46 +0100

    Ajout exercices

---
+++ exercices/ex1_variables.js
@@ -0,0 +1,11 @@
+function infosEtudiant(nom, note) {
+    if(note >= 10) {
+        return nom + " : " + note + "/20 → Admis";
+    } else {
+        return nom + " : " + note + "/20 → Ajourné";
+    }
+}
+
+// هنا كتكتبي console.log
+console.log(infosEtudiant("Hiba", 14));
+console.log(infosEtudiant("Ali", 8));
